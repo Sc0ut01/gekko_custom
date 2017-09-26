a0e29a36cade27c95ad6f27c1deea4203c3afebc
Sc0ut01 <32042443+Sc0ut01@users.noreply.github.com>
2017-09-26 21:17:44 +0200

    Update custom01.js

--- custom01.js
+++ custom01.js
@@ -5,31 +5,31 @@ var log = require('../core/log.js');
 
 //values
   //macd1
-global.macd1_short = 10;
-global.macd1_long = 21;
-global.macd1_signal = 9;
-global.macd1_down = -0.025;
-global.macd1_up = 0.025;
-global.macd1_persistence = 1;
+//global.macd1_short = 10;
+//global.macd1_long = 21;
+//global.macd1_signal = 9;
+//global.macd1_down = -0.025;
+//global.macd1_up = 0.025;
+//global.macd1_persistence = 1;
 
   //dema1
-global.dema1_short = 10;
-global.dema1_long = 28;
-global.dema1_down = -0.025;
-global.dema1_up = 0.025;
+//global.dema1_short = 10;
+//global.dema1_long = 28;
+//global.dema1_down = -0.025;
+//global.dema1_up = 0.025;
 
   //RSI
-global.rsi1_interval = 10;
-global.rsi1_low = 30;
-global.rsi1_high = 70;
-global.rsi1_persistence = 1;
+//global.rsi1_interval = 10;
+//global.rsi1_low = 30;
+//global.rsi1_high = 70;
+//global.rsi1_persistence = 1;
 
   //cci
-global.cci1_constant = 0.015;
-global.cci1_history = 90;
-global.cci1_up = 120;
-global.cci1_down = -30;
-global.cci1_persistance = 1;
+//global.cci1_constant = 0.015;
+//global.cci1_history = 90;
+//global.cci1_up = 120;
+//global.cci1_down = -30;
+//global.cci1_persistance = 1;
 
 // configuration
 var config = require('../core/util.js').getConfig();
@@ -50,17 +50,17 @@ method.init = function() {
 
   this.requiredHistory = config.tradingAdvisor.historySize;
   // define the indicators we need
-  var macd1_parameters = {short: global.macd1_short, long: global.macd1_long, signal: global.macd1_signal, down: global.macd1_down, up: global.macd1_up, persistence: global.macd1_persistence};
-  this.addIndicator('macd1', 'MACD', macd1_parameters);
+  //var macd1_parameters = {short: global.macd1_short, long: global.macd1_long, signal: global.macd1_signal, down: global.macd1_down, up: global.macd1_up, persistence: global.macd1_persistence};
+  this.addIndicator('macd1', 'MACD', this.settings);
   
-  var dema1_parameters = {short: global.dema1_short, long: global.dema1_short, down: global.dema1_down, up: global.dema1_up};
-  this.addIndicator('dema1', 'DEMA', dema1_parameters);
+  //var dema1_parameters = {short: global.dema1_short, long: global.dema1_short, down: global.dema1_down, up: global.dema1_up};
+  //this.addIndicator('dema1', 'DEMA', dema1_parameters);
   
-  var rsi1_parameters = {interval: global.rs1_interval, low: global.rs1_low, high: global.rs1_high, persistence: global.rs1_persistence};
-  this.addIndicator('rsi1', 'RSI', rsi1_parameters);
+  //var rsi1_parameters = {interval: global.rs1_interval, low: global.rs1_low, high: global.rs1_high, persistence: global.rs1_persistence};
+  //this.addIndicator('rsi1', 'RSI', rsi1_parameters);
   
-  var cci1_parameters = {constant: global.cci1_constant, history: global.cci1_history, up: global.cci1_up, down: global.cci1_down, persistence: global.cci1_persistence};
-  this.addIndicator('cci1', 'CCI', cci1_parameters);
+  //var cci1_parameters = {constant: global.cci1_constant, history: global.cci1_history, up: global.cci1_up, down: global.cci1_down, persistence: global.cci1_persistence};
+  //this.addIndicator('cci1', 'CCI', cci1_parameters);
   
   //this.addIndicator('ema21', 'EMA', 21);
   //this.addIndicator('ema34', 'EMA', 34);
@@ -97,7 +97,7 @@ method.log = function() {
 
 method.check = function() {
   var macd1_diff = this.indicators.macd1.result; 
-  if(macd1_diff > global.macd1_up) {
+  if(macd1_diff > this.settings.tresholds.up) {
     if(this.trend.direction !== 'up')
       this.trend = {
         duration: 0,
@@ -107,7 +107,7 @@ method.check = function() {
       };
   this.trend.duration++;
   log.debug('In uptrend since', this.trend.duration, 'candle(s)');
-    if(this.trend.duration >= global.macd1_persistence)
+    if(this.trend.duration >= this.settings.thresholds.persistence)
       this.trend.persisted = true;
     if(this.trend.persisted && !this.trend.adviced) {
       this.trend.adviced = true;
@@ -115,7 +115,7 @@ method.check = function() {
       } else
       this.advice(); 
  
-  }else if(macd1_diff < global.macd1_down) {
+  }else if(macd1_diff < this.settings.thresholds.down) {
     if(this.trend.direction !== 'down')
       this.trend = {
         duration: 0,
@@ -125,7 +125,7 @@ method.check = function() {
       };
     this.trend.duration++;
     log.debug('In downtrend since', this.trend.duration, 'candle(s)');
-    if(this.trend.duration >= global.macd1_persistence)
+    if(this.trend.duration >= this.settings.thresholds.persistence)
       this.trend.persisted = true;
     if(this.trend.persisted && !this.trend.adviced) {
       this.trend.adviced = true;
