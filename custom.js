3dd1143becc3e4fba81eb482daf488155753c4c5
Sc0ut01 <32042443+Sc0ut01@users.noreply.github.com>
2017-09-27 22:48:38 +0200

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,69 +1,47 @@
-// helpers
+// helpers-------------------------------------------------------------------------------------------------
 var _ = require('lodash');
 var log = require('../core/log.js');
-var globals = require('globals');
 
-//values
-  //macd1
-global.macd1_short = 10;
-global.macd1_long = 21;
-global.macd1_signal = 9;
-global.macd1_down = -0.025;
-global.macd1_up = 0.025;
-global.macd1_persistence = 1;
 
-  //dema1
-global.dema1_short = 10;
-global.dema1_long = 28;
-global.dema1_down = -0.025;
-global.dema1_up = 0.025;
-
-  //RSI
-global.rsi1_interval = 10;
-global.rsi1_low = 30;
-global.rsi1_high = 70;
-global.rsi1_persistence = 1;
-
-  //cci
-global.cci1_constant = 0.015;
-global.cci1_history = 90;
-global.cci1_up = 120;
-global.cci1_down = -30;
-global.cci1_persistance = 1;
-
-// configuration
+// configuration-------------------------------------------------------------------------------------------
 var config = require('../core/util.js').getConfig();
 // let's create our own method
 var method = {};
 
-// prepare everything our method needs
+// prepare everything our method needs---------------------------------------------------------------------
 method.init = function() {
   
   this.name = 'MACD_DEMA_RSI_CCI';
-  this.currentTrend;
+  this.trend = {
+    direction: 'none',
+    duration: 0,
+    persisted: false,
+    adviced: false
+  };
+
   this.requiredHistory = config.tradingAdvisor.historySize;
   // define the indicators we need
-  var macd1_parameters = {short: global.macd1_short, long: global.macd1_long, signal: global.macd1_signal, down: global.macd1_down, up: global.macd1_up, persistence: global.macd1_persistence};
+  var macd1_parameters = {short: 10, long: 21, signal: 9};
   this.addIndicator('macd1', 'MACD', macd1_parameters);
   
-  var dema1_parameters = {short: global.dema1_short, long: global.dema1_short, down: global.dema1_down, up: global.dema1_up};
+  var dema1_parameters = {short: 10, long: 21};
   this.addIndicator('dema1', 'DEMA', dema1_parameters);
   
-  var rsi1_parameters = {interval: global.rs1_interval, low: global.rs1_low, high: global.rs1_high, persistence: global.rs1_persistence};
-  this.addIndicator('rsi1', 'RSI', rsi1_parameters);
+  //var rsi1_parameters = {interval: global.rs1_interval, low: global.rs1_low, high: global.rs1_high, persistence: global.rs1_persistence};
+  //this.addIndicator('rsi1', 'RSI', rsi1_parameters);
   
-  var cci1_parameters = {constant: global.cci1_constant, history: global.cci1_history, up: global.cci1_up, down: global.cci1_down, persistence: global.cci1_persistence};
-  this.addIndicator('cci1', 'CCI', cci1_parameters);
+  //var cci1_parameters = {constant: global.cci1_constant, history: global.cci1_history, up: global.cci1_up, down: global.cci1_down, persistence: global.cci1_persistence};
+  //this.addIndicator('cci1', 'CCI', cci1_parameters);
   
   //this.addIndicator('ema21', 'EMA', 21);
   //this.addIndicator('ema34', 'EMA', 34);
   //this.addIndicator('ema144', 'EMA', 144);
   
   // initial value
-  this.lastLongPrice = 0;
+  //this.lastLongPrice = 0;
 }
 
-// what happens on every new candle?
+// what happens on every new candle?----------------------------------------------------------------------------
 method.update = function(candle) {
   // nothing!  
 }
@@ -71,13 +49,24 @@ method.update = function(candle) {
 // for debugging purposes: log the last calculated
 method.log = function() {
 
-  //var macd1_log = this.indicators.macd1;
-
 }
 
+//check----------------------------------------------------------------------------------------------------------
 method.check = function() {
+  
+  var macd1_down = -0.025;
+  var macd1_up = 0.025;
+  var macd1_persistence = 1;
+  
+  var dema1_down = -0.25;
+  var dema1_up = 0.25;
+  
+  var strat_sum = 0;
+  
+  //macd1-----------------------------------------------------------
+  
   var macd1_diff = this.indicators.macd1.result; 
-  if(macd1_diff > global.macd1_up) {
+  if(macd1_diff > macd1_up) {
     if(this.trend.direction !== 'up')
       this.trend = {
         duration: 0,
@@ -86,16 +75,17 @@ method.check = function() {
         adviced: false
       };
   this.trend.duration++;
-  log.debug('In uptrend since', this.trend.duration, 'candle(s)');
-    if(this.trend.duration >= global.macd1_persistence)
+  //log.debug('In uptrend since', this.trend.duration, 'candle(s)');
+    if(this.trend.duration >= macd1_persistence)
       this.trend.persisted = true;
     if(this.trend.persisted && !this.trend.adviced) {
       this.trend.adviced = true;
-      this.advice('long');
+      strat_sum = strat_sum + 2;
+      //this.advice('long');
       } else
       this.advice(); 
  
-  }else if(macd1_diff < global.macd1_down) {
+  }else if(macd1_diff < macd1_down) {
     if(this.trend.direction !== 'down')
       this.trend = {
         duration: 0,
@@ -104,22 +94,75 @@ method.check = function() {
         adviced: false
       };
     this.trend.duration++;
-    log.debug('In downtrend since', this.trend.duration, 'candle(s)');
-    if(this.trend.duration >= global.macd1_persistence)
+    //log.debug('In downtrend since', this.trend.duration, 'candle(s)');
+    if(this.trend.duration >= macd1_persistence)
       this.trend.persisted = true;
     if(this.trend.persisted && !this.trend.adviced) {
       this.trend.adviced = true;
-      this.advice('short');
+      strat_sum =strat_sum + 4;
+      //this.advice('short');
     } else
       this.advice();
 
   } else {
 
     log.debug('In no trend');
+    
+  }
   
-  
-  
- }
+  //dema1----------------------------------------------------------------------------  
+    
+  //var dema1_result = this.indicators.dema1;
+  var dema1_diff = this.indicators.dema1.result;
+  //var dema1_price = candle.close;
+
+  //var message = '@ ' + price.toFixed(8) + ' (' + diff.toFixed(5) + ')';
+
+  if(dema1_diff > dema1_up) {
+    //log.debug('we are currently in uptrend', message);
+
+    if(this.currentTrend !== 'up') {
+      this.currentTrend = 'up';
+      strat_sum =strat_sum + 8;
+      //this.advice('long');
+    } else
+      this.advice();
+
+  } else if(dema1_diff < dema1_down) {
+    //log.debug('we are currently in a downtrend', message);
+
+    if(this.currentTrend !== 'down') {
+      this.currentTrend = 'down';
+      strat_sum =strat_sum + 16;
+      //this.advice('short');
+    } else
+      this.advice();
+
+  } else {
+    log.debug('we are currently not in an up or down trend', message);
+    this.advice();
+  }
+
+  //odlocitev-------------------------------------------------------
+
+
+  if(this.currentTrend === 'long') {
+
+    if(strat_sum = 10) {
+      this.currentTrend = 'long';
+      this.advice('short')
+    }
+
+  } else if(this.currentTrend === 'short') {
+
+    if(strat_sum = 20) {
+      this.currentTrend = 'short';
+      this.advice('short')
+    }
+
+  }  
+ 
+ 
 }
 
 module.exports = method;
